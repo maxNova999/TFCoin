c93b40eaad85ec344195adf03b0c70d0b18eec58
maxNova999 <73742169+maxNova999@users.noreply.github.com>
2024-10-29 16:43:37 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+import {Telegraf, Markup} from 'telegraf'
+
+const token = '7538704808:AAEw3IZpnnwYLjuM2NAb3-YwOWffb5iJ94g'
+const webAppUrl = 'https://tfcoin-2de78.web.app'
+
+const bot = new Telegraf(token)
+
+bot.command('start', (ctx) => {
+  ctx.reply(
+    'Привет! Нажми чтобы начать играть!',
+    Markup.inlineKeyboard([
+      Markup.button.webApp(
+        'Начать',
+        `${webAppUrl}?ref=${ctx.payload}`
+      ),
+    ])
+  )
+})
+
+bot.launch()
