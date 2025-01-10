1c911b14dcc03eded3f5b2e09af3287944cd55ba
maxNova999 <73742169+maxNova999@users.noreply.github.com>
2025-01-11 00:36:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import {Telegraf, Markup} from 'telegraf'
+import supabase from './supabase.js'
 
 const token = '7538704808:AAEw3IZpnnwYLjuM2NAb3-YwOWffb5iJ94g'
 const webAppUrl = 'https://tfcoin-2de78.web.app'
@@ -17,4 +18,10 @@ bot.command('start', (ctx) => {
   )
 })
 
+setInterval(() => {
+  const res = supabase.from('users').update({energy: 10000})
+}, 20000)
+
+
 bot.launch()
+
